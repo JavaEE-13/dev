99355d9744ff516ea4163e65f946beca23735800
Oviea <41980406+Oviea@users.noreply.github.com>
2018-09-19 20:40:19 +0800

    Add files via upload

---
+++ Aluntan/src/java/jsf/EditorView.java
@@ -0,0 +1,17 @@
+package org.primefaces.showcase.view.input;
+ 
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class EditorView {
+     
+    private String text;
+ 
+    public String getText() {
+        return text;
+    }
+ 
+    public void setText(String text) {
+        this.text = text;
+    }
+}
